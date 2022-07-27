503a99369fad8ba6a2d6b3925f72235178829375
Yaznas <sanjshrestha53@gmail.com>
2022-07-27 17:58:30 +0545

    default sqlite db for now

--- jobsnp/settings.py
+++ jobsnp/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'jobsnp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'jobsnp',
-        'USER': 'sanj',
-        'PASSWORD': '123sanj53',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
